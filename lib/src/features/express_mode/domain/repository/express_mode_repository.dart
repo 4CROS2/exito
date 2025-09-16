7c09724703b9012cdd7e66219965ebacf54611cd
Yeferson Dev <yeferok2@gmail.com>
2025-09-16 11:44:04 -0500

    fix: Correct formatting in ExpressModeRepository interface definition

--- lib/src/features/express_mode/domain/repository/express_mode_repository.dart
+++ lib/src/features/express_mode/domain/repository/express_mode_repository.dart
@@ -1,4 +1,4 @@
-abstract interface class ExpressModeRepository  {
+abstract interface class ExpressModeRepository {
   Future<void> toggleExpressMode({required bool isEnabled});
   Future<bool> isExpressModeEnabled();
 }
